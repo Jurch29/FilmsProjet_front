43f49bdc71a042f69c128bdb315d4fa4bac82074
Jurch29 <julien.rch.29@gmail.com>
2020-01-02 23:34:17 +0100

    Mise en place architecture de base

---
+++ src/app/protected/protected.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { SharedModule } from 'src/app/shared/shared.module'; 
+
+import { ProtectedRoutingModule } from './protected-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    SharedModule,
+    ProtectedRoutingModule
+  ]
+})
+export class ProtectedModule { }
