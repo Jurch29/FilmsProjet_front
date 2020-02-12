16fb3a51246285aa6b1e1dc45faccfa80c5eeadb
MetalWarrior202 <root@BRUNELPierre.local>
2020-02-12 09:43:29 +0100

    FixingFilterBarLightMode

--- src/app/core/components/filterbar/filterbar.component.ts
+++ src/app/core/components/filterbar/filterbar.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { MovieService } from '../../service/movie-service.service';
 import { first } from 'rxjs/operators';
 import { Author } from 'src/app/shared/models/author';
 import { Category } from 'src/app/shared/models/category';
 import { Actor } from 'src/app/shared/models/actor';
+import { LightmodeService } from '../../service/lightmode.service';
 
 @Component({
   selector: 'app-filterbar',
@@ -11,16 +12,25 @@ import { Actor } from 'src/app/shared/models/actor';
   styleUrls: ['./filterbar.component.css']
 })
 
-export class FilterbarComponent implements OnInit {
+export class FilterbarComponent implements OnInit,OnDestroy {
   categorys  =new Array<Category>();
   actors  =new Array<Actor>();
   authors =new Array<Author>();
-  constructor(private movieService : MovieService) { }
+  subscriptionlightMode: any;
+  lightMode: boolean;
+  constructor(private lightmodeService:LightmodeService,private movieService : MovieService) { }
 
   ngOnInit() {
     this.movieService.getAllAuthors().pipe(first()).subscribe(data => this.authors = data);
     this.movieService.getAllCategorys().pipe(first()).subscribe(data => this.categorys = data);
     this.movieService.getAllActors().pipe(first()).subscribe(data => this.actors = data);
+    this.subscriptionlightMode =  this.lightmodeService.getLightModeEventMessage().subscribe(value =>
+      this.lightMode = value
+    );
+   
+  }
+  ngOnDestroy(): void {
+    this.subscriptionlightMode.unsubscribe();
   }
   onOrderByChange(event){
       console.log("event = "+event.value);//If undefined means choose no selection
