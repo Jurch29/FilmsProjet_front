9209112af9b1c6fef69a0bd532fbe41df9b7dab4
abergnel <anthony.bergnel@gmail.com>
2020-03-05 11:12:41 +0100

    fix erreur synopsis

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   //apiUrl: 'http://localhost:8080/api'
-  apiUrl: 'http://192.168.56.1:8080/api'
+  apiUrl: 'http://localhost:8080/api'
 };
 
 /*
