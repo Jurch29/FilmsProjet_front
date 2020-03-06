991fa702c8eb8b3b9730642d386c0d4fed44a066
MetalWarrior202 <pierre.brunel.fr@gmail.com>
2020-03-06 14:38:36 +0100

    CSSFIX4

--- src/app/protected/administration/administration/administration.component.ts
+++ src/app/protected/administration/administration/administration.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { LightmodeService } from 'src/app/core/service/lightmode.service';
 
 @Component({
   selector: 'app-administration',
   templateUrl: './administration.component.html',
+  encapsulation: ViewEncapsulation.None,
   styleUrls: ['./administration.component.css']
 })
 export class AdministrationComponent implements OnInit {
